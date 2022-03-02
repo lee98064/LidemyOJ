f9a96c1140bdc77691f9bcf52eec6be2876b1b74
Ryan Lee <lee98064@gmail.com>
2022-03-03 01:48:51 +0800

    Fix 1048 Array Bug

--- 1048.py
+++ 1048.py
@@ -6,12 +6,12 @@ for _ in range(n):
 
 item_sum = [0] * n
 
-for i in range(1,n,1):
+for i in range(0,n,1):
     item_sum[i] = item[i] + item_sum[i-1]
 
 ans = item[0]
 
-for i in range(1,n,1):
+for i in range(0,n,1):
     for j in range(i,n,1):
         s = item_sum[j] - item_sum[i - 1]
         if(s > ans):
