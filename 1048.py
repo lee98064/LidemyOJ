b10e90b3fb6cd9d4af01962726968c56670b2266
Ryan Lee <lee98064@gmail.com>
2022-03-03 01:46:31 +0800

    Finish 1048

---
+++ 1048.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+item = []
+for _ in range(n):
+    item.append(int(input()))
+
+item_sum = [0] * n
+
+for i in range(1,n,1):
+    item_sum[i] = item[i] + item_sum[i-1]
+
+ans = item[0]
+
+for i in range(1,n,1):
+    for j in range(i,n,1):
+        s = item_sum[j] - item_sum[i - 1]
+        if(s > ans):
+            ans = s
+
+print(ans)
